677715401a10d8d0448168f596dc035a48ec6d71
Mathias <mathiasmichaelweiss@gmail.com>
2020-09-13 09:40:47 +0200

    new_class

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,16 @@ const menuItem = document.querySelectorAll('.nav__link'),
     closeModal = document.querySelectorAll('.modal__close__item'),
     btn = document.querySelectorAll('#btn');
 
+/* function scrollSetup (elem) {
+    elem.scrollIntoView({block: 'center', behavior: 'smooth'});
+}
+
+btn.addEventListener('click', () => {
+    handleButtonClick(menuItem[1]);
+}); */
+
+
+
 btn.forEach(item => {
     item.addEventListener('mouseover', () => {
         item.style.cssText = `
@@ -44,23 +54,6 @@ closeModal.forEach(item => {
     });
 });
 
-
-skillImage.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.cssText = `
-            transition: 0.5s;
-            width: 110%;
-            height: 110%;
-        `;
-
-    });
-    item.addEventListener('mouseout', () => {
-        item.style.width = '';
-        item.style.height = '';
-        item.style.transition = '0.5';
-    });
-});
-
 workLink.forEach(item => {
     item.addEventListener('mouseover', () => {
         item.style.cssText = `text-decoration-line: underline;`;
@@ -89,3 +82,59 @@ lang.forEach(item => {
     });
 
 });
+
+// skills class
+class Skills {
+    constructor(src, title, parentSelector, lvl) {
+        this.src = src;
+        this.title = title;
+        this.parent = document.querySelector(parentSelector);
+        this.lvl = lvl;
+    }
+    render() {
+        const element = document.createElement('div');
+        element.classList.add('skills__item');
+
+        element.innerHTML = `
+            <div class="skill__icon">
+                <img src=${this.src} alt="" class="skill__img">
+            </div>
+            <h4 class="skill__title">${this.title}</h4>
+            <div class="skill__level">
+                <img src=${this.lvl} alt="">
+            </div>
+        `;
+
+        this.parent.append(element);
+    }
+}
+
+new Skills(
+    '/src/img/icons/first__icon.png',
+    'HTML5',
+    '.skills',
+    '/src/img/icons/4_skill.png'
+).render();
+
+new Skills(
+    '/src/img/icons/second__icon.png',
+    'CSS3',
+    '.skills',
+    '/src/img/icons/4_skill.png'
+).render();
+
+new Skills(
+    '/src/img/icons/third__icon.png',
+    'JavaScript',
+    '.skills',
+    '/src/img/icons/4_skill.png'
+).render();
+
+new Skills(
+    '/src/img/icons/fourth__icon.png',
+    'React',
+    '.skills',
+    '/src/img/icons/4_skill.png'
+).render();
+
+// ______________ // 
